1e9b43a298095a9ec2ac2bbc84990da3e7c83172
Pallavi.R <pallavimanjula30@gmail.com>
2025-02-10 06:04:31 +0530

    2nd commit

---
+++ Javaattnsif/src/org/tnsif/accenture/c2tc/basic/Array.java
@@ -0,0 +1,18 @@
+package org.tnsif.accenture.c2tc.basic;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int num[]=new int[4];
+	     num[0]=23;
+	     num[1]=45;
+	     num[2]=56;
+	     
+	     System.out.println(num[0]);
+	     System.out.println(num[1]);
+	     System.out.println(num[2]);
+
+	}
+
+}
